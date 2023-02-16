7a94495b355336441d2bce210315bc4a0cdf2267
bhumika-nayak <113764065+bhumika-nayak@users.noreply.github.com>
2023-02-16 12:26:29 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I, Bhumika Nayak(PES1UG29CS104) have successfully built and run\n";
+  cout << "This is the change made\n";
   return 0;
 }
