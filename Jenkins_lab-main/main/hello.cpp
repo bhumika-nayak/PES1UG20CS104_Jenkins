ac0bda52a7cf0ecfeefe97f8bad618bdb279f0bc
bhumika-nayak <113764065+bhumika-nayak@users.noreply.github.com>
2023-02-16 11:33:23 +0530

    Add files via upload

---
+++ Jenkins_lab-main/main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I, Bhumika Nayak(PES1UG29CS104) have successfully built and run\n";
+  return 0;
+}
